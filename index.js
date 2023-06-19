488161c0fa7f12e5d4b99a084ea7e50a99562f3f
midzer <midzer@gmail.com>
2023-06-19 21:29:07 +0200

    fix header

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use((req, res, next) => {
   // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000"); // YOUR-DOMAIN.TLD
+  res.header("Access-Control-Allow-Origin", "https://face4peace.com"); // YOUR-DOMAIN.TLD
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === "OPTIONS") {
